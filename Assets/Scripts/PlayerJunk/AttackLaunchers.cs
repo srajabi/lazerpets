d5d63737afba6969b0d0ffea9f496a1162661bf0
Jeremy Burns <j@jaburns.net>
2019-01-26 15:06:03 -0700

    Projectile

---
+++ Assets/Scripts/PlayerJunk/AttackLaunchers.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum AttackKind
+{
+    CatProjectile,
+    DogPuke,
+    BirdPoop,
+}
+
+public interface IAttackLauncher
+{
+    void Update(bool attackButtonPressed, Vector3 position, Vector3 direction);
+}
+
+static public class AttackLauncherFactory
+{
+    static public IAttackLauncher Create(AttackKind kind)
+    {
+        switch (kind) 
+        {
+            case AttackKind.CatProjectile:
+                return new CatProjectileLauncher();
+        }
+
+        return null;
+    }
+}
+
+public class CatProjectileLauncher : IAttackLauncher
+{
+    bool buttonDown = false;
+
+    public void Update(bool attackButtonPressed, Vector3 position, Vector3 direction)
+    {
+        if (attackButtonPressed && !buttonDown)
+        {
+            var pukeObj = Object.Instantiate(Resources.Load<GameObject>("Prefabs/CatProjectile"), position, Quaternion.identity);
+            pukeObj.GetComponent<CatProjectileController>().Init(direction);
+            buttonDown = true;
+        }
+
+        if (!attackButtonPressed)
+            buttonDown = false;
+    }
+}
---
+++ Assets/Scripts/PlayerJunk/CatProjectileController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CatProjectileController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public void Init(Vector3 direction)
+    {
+        transform.position += direction * 0.1f;
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = direction * 5f;
+        StartCoroutine(delayedEnableColliderRoutine());
+    }
+
+    IEnumerator delayedEnableColliderRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<Collider>().enabled = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
