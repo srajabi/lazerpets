0bfb6c14a43231d8cfed307c455013822360867b
Cody Ward <cody.ward@scopear.com>
2019-01-26 12:01:33 -0700

    Made a player prefab, and added a game spawner

---
+++ Assets/Scripts/Game/Characters/CharacterTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace Game
+{
+    public enum CharacterTypes
+    {
+        Unassigned = 0,
+        Cat  = 1,
+        Dog  = 2,
+        Bird = 3,
+    }
+}
---
+++ Assets/Scripts/Game/GameManager/GameSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Game
+{
+    public class GameSpawner
+    {
+        private readonly GameManager manager;
+
+        public GameSpawner(GameManager manager)
+        {
+            this.manager = manager;
+        }
+
+        public void Spawn(Player player)
+        {
+            player.transform.position = SpawnPointManager.GetRandomVacantPoint(player.CharacterType).transform.position;
+        }
+
+        public void ReSpawn(Player player)
+        {
+            player.transform.position = SpawnPointManager.GetFurthestPoint(player.CharacterType, manager.Players.Where(p=>p!=player).Select(p=>p.transform).ToArray()).transform.position;
+        }
+    }
+}
