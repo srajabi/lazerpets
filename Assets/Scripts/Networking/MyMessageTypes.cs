67f6a50087fdf6bfa768d75e4d1411ffb63301ed
Sohail Rajabi <srajabi@gmail.com>
2019-01-26 02:50:38 -0700

    small networking example

---
+++ Assets/Scripts/Networking/MyMessageTypes.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine.Networking;
+
+public static class MyMessageTypes
+{
+    public const int HeartBeat = MsgType.Highest + 1;
+    public const int Strings = MsgType.Highest + 2;
+}
