740a5d4cdaac96884620103c2fe8da0596808035
Jeremy Burns <j@jaburns.net>
2019-01-25 21:31:07 -0700

    Fixed weird broken cat controller state

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.up * Time.deltaTime;
     }
 }
