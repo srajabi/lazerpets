e8ecb080102a9bbefa03a57ae744b6400211b5a8
adam.sanche <hooberschmit@gmail.com>
2019-01-26 11:50:57 -0700

    add a boot scene, add ui prefabs etc...

---
+++ Assets/Scripts/Boot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Boot : MonoBehaviour
+{
+    private void Start()
+    {
+        SceneManager.LoadScene("MainLevel");
+        var uiPrefab = Resources.Load<CanvasController>("Prefabs/UI");
+        var gameCameraPrefab = Resources.Load<Camera>("Prefabs/GameCamera");
+        var tvCameraPrefab = Resources.Load<Camera>("Prefabs/TVCamera");
+
+        var ui = Instantiate(uiPrefab);
+        var gameCamera = Instantiate(gameCameraPrefab);
+        var tvCamera = Instantiate(tvCameraPrefab);
+
+        ui.Initialize(gameCamera, tvCamera);
+    }
+}
