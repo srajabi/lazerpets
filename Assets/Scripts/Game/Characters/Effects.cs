9ec50d01119bd592c863da3aac266eb1bb0c7184
Sohail Rajabi <srajabi@gmail.com>
2019-01-26 16:23:13 -0700

    damage effect

---
+++ Assets/Scripts/Game/Characters/Effects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class Effects : MonoBehaviour
+    {
+        public ParticleSystem damageEffect;
+
+        public void InstantiateDamageEffect(float lifeTime)
+        {
+            Instantiate(damageEffect);
+            Destroy(damageEffect, lifeTime);
+        }
+    }
+}
