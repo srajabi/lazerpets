d57a1c967c4f68356c0f7db561e19df1ea48741b
Brent Housen <brent@scopear.com>
2019-01-26 23:28:59 -0700

    Tweaked the horse tv.

---
+++ Assets/Scripts/UI/MaterialShaderOffsetAnimator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialShaderOffsetAnimator : MonoBehaviour
+{
+    // Scroll main texture based on time
+
+    [SerializeField]
+    Vector2 ScrollOffset;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 offset = Time.time * ScrollOffset;
+        rend.material.SetTextureOffset("_MainTex", offset);
+    }
+}
