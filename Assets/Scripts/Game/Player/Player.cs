a5f451bb885a232bb680120400c83bd407492708
Cody Ward <cody.ward@scopear.com>
2019-01-26 18:56:52 -0700

    Character Type spawning

--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -17,8 +17,6 @@ namespace Game
         {
             Health = GetComponentInChildren<Health>(true);
             Score = GetComponentInChildren<PlayerScore>(true);
-            CritterController = GetComponentInChildren<CritterController>(true);
-            Effects = GetComponentInChildren<Effects>(true);
         }
 
         internal void Initialize(NetworkPlayer netPlayer)
@@ -28,6 +26,10 @@ namespace Game
             netPlayer.Player = this;
 
             Debug.Log("Player Initialized: " + name + " isSelf" + netPlayer.IsSelf);
+
+            GetComponent<CharacterInstantiator>().Create();
+            CritterController = GetComponentInChildren<CritterController>(true);
+            Effects = GetComponentInChildren<Effects>(true);
         }
     }
 }
